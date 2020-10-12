5394e36b8e88ed09fd1a80f829771d7dd728c249
Jeff Dalley <jeffdalley@gmail.com>
2020-10-11 22:32:09 -0500

    Module 4: Headless CMS

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // Variable Name
+  name: 'topping',
+  // Visible Title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is this a vegetarian topping?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
