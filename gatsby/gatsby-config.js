47a83ac7a9c0c932428ac77210e1282488699464
Jeff Dalley <jeffdalley@gmail.com>
2020-10-13 23:08:48 -0500

    Module 5: Data into Gatsby with GraphQL

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,24 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: 'Slicks Slices',
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Hamilton!',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // Name of the plugin
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '6m7tl78v',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
